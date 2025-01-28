48b5690360c578881090ad7816ac3e072f93bf20
ssuminnnn <133936963+ssuminnnn@users.noreply.github.com>
2025-01-28 21:38:28 +0900

    baekjoon_3046
    백준 3046 R2 25.01.28.
    
    Signed-off-by: ssuminnnn <133936963+ssuminnnn@users.noreply.github.com>

---
+++ baekjoon_3046.c
@@ -0,0 +1,13 @@
+//  baekjoon_3046 R2 25.01.28.
+#include <stdio.h>
+int main(void)
+{
+    int r1;
+    int r2;
+    int avg;
+    
+    scanf("%d %d",&r1,&avg);
+    r2=(avg*2)-r1;
+    printf("%d",r2);
+    return 0;
+}
